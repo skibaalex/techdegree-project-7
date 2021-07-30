243df3c61aa65c77701ffe32b7b3c5349ed0b6b7
Alexander Skiba <skibaalex1@gmail.com>
2021-07-30 09:22:39 -0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -9,8 +9,8 @@ const NotFound = ({handleSearchSubmit}) => {
         <>
             <SearchForm handleSubmitSearch={handleSearchSubmit} />
             <Nav />
-            <Image src="https://media3.giphy.com/media/AKKhWXbQrmXFdOFNDf/giphy.gif?cid=ecf05e476pgzzx6yhxmsl20p1sood6cuihpjc0g30m5mzyz1&rid=giphy.gif&ct=g" alt="Oops Not found" />
             <h2 className="h2">404 PAGE NOT FOUND</h2>
+            <Image src="https://media3.giphy.com/media/AKKhWXbQrmXFdOFNDf/giphy.gif?cid=ecf05e476pgzzx6yhxmsl20p1sood6cuihpjc0g30m5mzyz1&rid=giphy.gif&ct=g" alt="Oops Not found" />
         </>
     )
 }
